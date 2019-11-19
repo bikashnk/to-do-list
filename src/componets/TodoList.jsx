98a2cb393f7fa93d4fd0610cb349f9ff05dae126
Bikash Ranjan <r.bikashnk@gmail.com>
2019-11-19 12:23:46 +0530

    Modified To Functional Component

--- src/componets/TodoList.jsx
+++ src/componets/TodoList.jsx
@@ -6,48 +6,22 @@ import UserProfile from './UserProfile.jsx';
 import ListDetails from './ListDetails.jsx';
 import '../style/TodoList.css';
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      players: [],
-      result: {},
-    }
-  }
-  setPlayerMode = list => {
-    this.setState({ players: list });
-  }
-  getResult = result => {
-    this.setState({ result });
-  }
-  resetState = confirm => {
-    if (confirm === "Playagain") {
-      this.setState({ result: {} });
-    } else {
-      this.setState({
-        players: [],
-        result: {},
-      });
-    }
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <ToDoHeader />
-        <div className="container" style={{ marginTop: '1em' }}>
-          <div className="row">
-            <div className="col-lg-4">
-              <UserProfile />
-            </div>
-            <div className="col-lg-8">
-              <ListDetails/>
-            </div>
+const TodoList = () => {
+  return (
+    <React.Fragment>
+      <ToDoHeader />
+      <div className="container" style={{ marginTop: '1em' }}>
+        <div className="row">
+          <div className="col-lg-4">
+            <UserProfile />
+          </div>
+          <div className="col-lg-8">
+            <ListDetails />
           </div>
         </div>
-      </React.Fragment>
-    )
-  }
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default TodoList;
